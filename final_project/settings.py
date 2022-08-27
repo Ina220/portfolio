b78e5591adb93d68b6b45b596f76078fc7d04385
Ina220 <cvetms8@gmail.com>
2022-08-27 15:50:19 -0600

    update

--- final_project/settings.py
+++ final_project/settings.py
@@ -122,9 +122,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_URL = '/capstone/static/'
 
-MEDIA_URL = '/capstone/media/'
+MEDIA_URL = '/media/'
 
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static')
