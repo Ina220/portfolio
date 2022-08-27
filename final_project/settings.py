c5be0e4902e11f42131bea91a27ca07c0bcd7c31
Ina220 <cvetms8@gmail.com>
2022-08-27 15:47:07 -0600

    modified

--- final_project/settings.py
+++ final_project/settings.py
@@ -13,9 +13,10 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 import os
 from pathlib import Path
 
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-STATIC_ROOT = BASE_DIR / 'staticfiles'
+#STATIC_ROOT = BASE_DIR / 'staticfiles'
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
@@ -123,7 +124,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
-MEDIA_URL = '/media/'
+MEDIA_URL = '/capstone/media/'
 
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static')
